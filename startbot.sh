9b832c11246b85e3787888fda3606c56dc918f2c
John Young <jyoung@bauerxcel.com>
2016-12-16 19:20:22 -0500

    Adding a startup script with config

---
+++ startbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# General configuration
+export HUBOT_HEROKU_KEEPALIVE_URL="https://tikarobot.herokuapp.com/"
+# export HUBOT_LOG_LEVEL="debug"
+# export HEROKU_URL="https://bauerbot-production.herokuapp.com"
+
+bin/hubot
